ae9f7e4ee7c0f33e3fa4f9a372d227a49d47e764
janeishahu <janeishahu@aol.com>
2021-09-13 21:40:36 -0400

    finished part 1

---
+++ hello-you/src/components/Bio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+    <div className='bio'>
+        <h1 className= 'title'> Janeisha Hutley </h1>
+        <p className= 'bio-text'> I am a forensic scientist from philly pursuing a career in software development. </p>
+    </div>
+
+    )
+}
+
+export default Title 
